60dfbf9ad21aab7501b730bdcd21ef5d7fec7b60
ethemdenizersoy16 <ethemdenizersoy@gmail.com>
2026-04-19 15:38:20 +0300

    fix: Added render link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -73,7 +73,7 @@ function App() {
   useEffect(() => {
     const fetchHistory = async () => {
       try {
-        const response = await fetch(`http://127.0.0.1:8000/api/history?session_id=${sessionId}`);
+        const response = await fetch(`https://finchat-zui4.onrender.com/api/history?session_id=${sessionId}`);
         const data = await response.json();
 
         if (data.history && data.history.length > 0) {
@@ -144,7 +144,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/api/chat", {
+      const response = await fetch("https://finchat-zui4.onrender.com/api/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
